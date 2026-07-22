f719ccc9f12469eb1f9ab33e8efb4271e1fe32fd
ALEX MARIO PORTILLO LAUPA <aportillol@juntos.gob.pe>
2026-07-22 18:54:37 -0500

    20260722-1854

--- scripts/05_medidas_asociacion_epi.R
+++ scripts/05_medidas_asociacion_epi.R
@@ -1,9 +1,19 @@
 library(epiR)
 library(gt)
 library(dplyr)
+library(survival)
+library(ggfortify)
+library(ggplot2)
+library(patchwork)
+library(tidyverse)
+library(survminer)
+library(broom)
+library(discSurv)
+library(ggsci)
+library(DT)
+library(broom)
 
 data <- readRDS("data/data_transformada.rds")
-
 
 #Razón de incidencias acumuladas de recurrencia de cáncer
 #==============================================================================
@@ -104,3 +114,150 @@ gtsave(
   vheight = 700,
   expand = 10
 )
+
+#Kaplan Meier
+#==============================================================================
+data_km <- data %>%
+  mutate(
+    status = case_when(
+      Recurrence == "Presence" ~ 1,
+      Recurrence == "Absence" ~ 0,
+      TRUE ~ NA_real_
+    )
+  ) %>%
+  filter(
+    !is.na(Follow_up_month),
+    !is.na(status)
+  )
+
+table(data_km$Recurrence, data_km$status)
+
+# Cohorte general
+km_general <- survfit(Surv(Follow_up_month, status) ~ 1, data = data_km)
+#tidy(km_general)
+#tail(tidy(km_general), 15)
+
+# Según CDKN2A tumoral
+km_cdkn2a <- survfit(Surv(Follow_up_month, status) ~ CDKN2A_T, data = data_km)
+
+# Según MGMT tumoral
+km_mgmt <- survfit(Surv(Follow_up_month, status) ~ MGMT_T, data = data_km)
+
+# Según MLH1 tumoral
+km_mlh1 <- survfit(Surv(Follow_up_month, status) ~ MLH1_T, data = data_km)
+
+#Creación de gráficos de Kaplan Meier: General y por tipo de gen
+grafico_km_general <- autoplot(km_general, fun= "event", surv.colour = "black") + 
+  labs(x = "Tiempo (meses)",
+       y = "Probabilidad acumulada de recurrencia",
+       title = "Cohorte general") +
+  coord_cartesian(xlim = c(0, 80)) +
+  theme_minimal()
+
+grafico_km_cdkn2a <- autoplot(km_cdkn2a, fun= "event", surv.alpha = 0.8, conf.int.alpha = 0.4) +
+  labs(x = "Tiempo (meses)",
+       y = "Probabilidad acumulada de recurrencia",
+       title = "Gen CDKN2A en tejido tumoral") +
+  scale_fill_lancet() +
+  scale_color_lancet() +
+  coord_cartesian(xlim = c(0, 80)) +
+  theme_minimal()
+
+grafico_km_mgmt <- autoplot(km_mgmt, fun= "event", surv.alpha = 0.8, conf.int.alpha = 0.4) +
+  labs(x = "Tiempo (meses)",
+       y = "Probabilidad acumulada de recurrencia",
+       title = "Gen MGMT en tejido tumoral") +
+  scale_fill_lancet() +
+  scale_color_lancet() +
+  coord_cartesian(xlim = c(0, 80)) +
+  theme_minimal()
+
+grafico_km_mlh1 <- autoplot(km_mlh1, fun= "event", surv.alpha = 0.8, conf.int.alpha = 0.4) +
+  labs(x = "Tiempo (meses)",
+       y = "Probabilidad acumulada de recurrencia",
+       title = "Gen MLH1 en tejido tumoral") +
+  scale_fill_lancet() +
+  scale_color_lancet() +
+  coord_cartesian(xlim = c(0, 80)) +
+  theme_minimal()
+
+grafico_km_completo <-
+  (grafico_km_general | grafico_km_cdkn2a) /
+  (grafico_km_mgmt | grafico_km_mlh1) +
+  plot_annotation(
+    title = paste(
+      "Probabilidad acumulada de recurrencia según",
+      "el estado de metilación en tejido tumoral"
+    )
+  )
+
+grafico_km_completo
+
+ggsave(
+  filename = "outputs/figuras/kaplan_meier_general_y_genes.png",
+  plot = grafico_km_completo,
+  width = 14,
+  height = 10,
+  dpi = 300
+)
+
+#Long-rank: Determinar si existen diferencias entre las curvas
+
+#CDKN2A
+logrank_cdkn2a <- survdiff(
+  Surv(Follow_up_month, status) ~ CDKN2A_T,
+  data = data_km
+)
+
+p_cdkn2a <- 1 - pchisq(
+  logrank_cdkn2a$chisq,
+  df = length(logrank_cdkn2a$n) - 1
+)
+
+logrank_cdkn2a
+p_cdkn2a
+
+#MGMT
+logrank_mgmt <- survdiff(
+  Surv(Follow_up_month, status) ~ MGMT_T,
+  data = data_km
+)
+
+p_mgmt <- 1 - pchisq(
+  logrank_mgmt$chisq,
+  df = length(logrank_mgmt$n) - 1
+)
+
+logrank_mgmt
+p_mgmt
+
+#MLH1
+logrank_mlh1 <- survdiff(
+  Surv(Follow_up_month, status) ~ MLH1_T,
+  data = data_km
+)
+
+p_mlh1 <- 1 - pchisq(
+  logrank_mlh1$chisq,
+  df = length(logrank_mlh1$n) - 1
+)
+
+logrank_mlh1
+p_mlh1
+
+#Resumen
+resultado_logrank <- data.frame(
+  Gen = c("CDKN2A", "MGMT", "MLH1"),
+  Chi_cuadrado = c(
+    logrank_cdkn2a$chisq,
+    logrank_mgmt$chisq,
+    logrank_mlh1$chisq
+  ),
+  p_valor = c(
+    p_cdkn2a,
+    p_mgmt,
+    p_mlh1
+  )
+)
+
+resultado_logrank
